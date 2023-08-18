9db649257ea01421dac0032e226fed7ac65bab6b
Andres Lora <jalc.andres.lora@gmail.com>
2023-08-17 18:57:25 -0700

    created CardList and Card components

---
+++ src/containers/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import CardList from "../components/CardList";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      robots: [],
+      searchfield: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ robots: users });
+      });
+  }
+
+  render() {
+    const { robots } = this.state;
+    return !robots.length ? (
+      <h1>Loading</h1>
+    ) : (
+      <div className="tc">
+        <h1 className="f1">RoboFriends</h1>
+        <CardList robots={robots} />
+      </div>
+    );
+  }
+}
+
+export default App;
